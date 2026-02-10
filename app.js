5b7d0359f63e9982e26da385c8af33ab6ca1b521
Neith <gulaneithandeniel@gmail.com>
2026-02-10 20:22:03 +0800

    changes app and api

--- app.js
+++ app.js
@@ -1,31 +1,24 @@
 const express = require('express');
-const serverless = require('serverless-http');
 const morgan = require('morgan');
-const path = require('path');
 
 const app = express();
 
-// Middleware
+app.set('view engine', 'ejs');
+app.use(express.static("public"))
 app.use(morgan('dev'));
 
-// IMPORTANT: Fix static file serving for Serverless
-// This points to the 'public' folder relative to where this function runs
-app.use(express.static(path.join(__dirname, '../public')));
-
-// Set view engine
-app.set('view engine', 'ejs');
-// IMPORTANT: Point to 'views' relative to this function
-app.set('views', path.join(__dirname, '../views'));
-
-// Routes
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-// 404 Handler
-app.use((req, res) => {
-    res.status(404).render('404', {title: '404 - Not Found'});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
 
-// Export the handler for Netlify
-module.exports.handler = serverless(app);
+
+app.use((req, res) => {
+    res.status(404).render('404', {title: '404'})
+});
+
+
