95652a35a7af4a615a60c9dde3fa04b0d44ef2cf
Neith <gulaneithandeniel@gmail.com>
2026-04-02 22:13:44 +0800

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -51,21 +51,20 @@ app.get('/updates/internship', (req, res) => {
                     ""
                 ],
                 images: [
-                    "https://placehold.co/600x400/354c7c/eaebfe?text=Site+Hunting+Photo+1",
-                    "https://placehold.co/600x400/354c7c/eaebfe?text=Site+Hunting+Photo+2"
+                    
                 ],
-                // Place your funny gif here!
                 gif: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNXZuODVqMjAxaG9wazkzYjZxZXJha3NnajdnZGF1cWJsMzNqNnYyayZlcD12MV9naWZzX3NlYXJjaCZjdD1n/08uBcURaMq6vA93TGc/giphy.gif"
             },
             {
                 week: "Week 2",
-                date: "Feb 23 - Feb 27, 2026",
-                title: "Awaiting Placement...",
+                date: "Feb 25 - Feb 27, 2026",
+                title: "Official Intern of St.Joseph",
                 content: [
-                    "I will be updating this space as I secure a site and progress through the internship. Documenting the challenges faced and the solutions developed along the way. Stay tuned."
+                    "During this period, the focus was on the initial planning phase for the \"Calendar of Events\" school system. My intern team collaborated using Google Docs to brainstorm and draft a detailed questionnaire intended to gather functional requirements from school administration. This draft was prepared for a supervisory consultation with Sir Jv (John Venneth S. Bartolome) to ensure the project scope aligns with the IT department's specific needs.",
+                    "A significant administrative milestone was reached on Friday, February 27, with the formal signing of the Memorandum of Understanding (MOU). This event officially transitioned the participants from an orientation stage to recognized intern roles within the Saint Joseph School Foundation. In terms of logistics, 14 hours were logged during the week, bringing the cumulative total for the internship to 21 hours."
+
                 ],
                 images: [],
-                // Place your funny gif here!
                 gif: "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExaWhpM2c0bDNpNndtbXY3dTJjY2U0NjgzdHF5M25kZTMxczQ1cmowYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xpI9kszfSCMQAr4wmS/giphy.gif"
             }
         ]
